49ef18b18e5ee14d6bd6825c43855c405390f0db
Madhur82954 <itzmadhurverma@gmail.com>
2023-06-14 12:53:02 +0530

    [Madhur] - UC13-LambdaFunction Added

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -5,31 +5,11 @@ namespace UserRegistrationProblem
 {
     public class Program
     {
-        public bool validFirstName(string Firstname)
-        {
-            string Firstnamepattern = "^[A-Z]{1}[a-z]{2,}$";
-            return Regex.IsMatch(Firstname, Firstnamepattern);
-        }
-        public bool validLastName(string Lastname)
-        {
-            string Lastnamepattern = "^[A-Z]{1}[a-z]{2,}$";
-            return Regex.IsMatch(Lastname, Lastnamepattern);
-        }
-        public bool validPhoneno(string Phoneno)
-        {
-            string Phonenopattern = "^[0-9]{2,}[ ][0-9]{10}$";
-            return Regex.IsMatch(Phoneno, Phonenopattern);
-        }
-        public bool validEmail(string Email)
-        {
-            string Emailpattern = "^[0-9a-zA-Z]+[.+_-]{0,2}[0-9a-zA-Z]+[@][0-9a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
-            return Regex.IsMatch(Email, Emailpattern);
-        }
-        public bool validPassword(string Password)
-        {
-            string Passwordpattern = ("^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[a-z])(?=.*?[#?!@$%^&*-]).{8,}$");
-            return Regex.IsMatch(Password, Passwordpattern); ;
-        }
+        public Func<string, bool> validFirstName = Firstname => Regex.IsMatch(Firstname, "^[A-Z]{1}[a-z]{2,}$");
+        public Func<string, bool> validLastName = Lastname => Regex.IsMatch(Lastname, "^[A-Z]{1}[a-z]{2,}$");
+        public Func<string, bool> validPhoneno = Phoneno => Regex.IsMatch(Phoneno, "^[0-9]{2,}[ ][0-9]{10}$");
+        public Func<string, bool> validEmail = Email => Regex.IsMatch(Email, "^[0-9a-zA-Z]+[.+_-]{0,2}[0-9a-zA-Z]+[@][0-9a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$");
+        public Func<string, bool> validPassword = Password => Regex.IsMatch(Password, "^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[a-z])(?=.*?[#?!@$%^&*-]).{8,}$");
         static void Main(string[] args)
         {
             Console.WriteLine("Enter FirstName : ");
